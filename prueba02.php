a4d9cdb41f629ee748ad7f13d66f7a7868c8b08e
DeusElí <deuseli@localhost.localdomain>
2022-08-16 18:14:19 -0600

    Práctica completada

---
+++ prueba02.php
@@ -0,0 +1,19 @@
+<?php include "includes/header.php"; ?>
+
+<?php
+echo "Hola mundo";
+echo "</br>";
+echo ("Hola mundo");
+echo "</br>";
+print_r("Hola mundo");
+echo "</br>";
+var_dump("Hola mundo");
+
+echo "</br>";
+$nombre = "Kathe";
+echo "Hola, <b> $nombre </b>, bienvenida a la clase";
+echo "</br>";
+echo "Hola, " . $nombre . ", gusto de verte";
+?>
+
+<?php include "includes/footer.php"; ?>
